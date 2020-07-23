d07e3ee648e7d34e70f41673b6e7b09ce26a9d08
lefuq <lefuq@DESKTOP-06BGU16.localdomain>
2020-07-23 23:35:55 +0700

    added api config

--- server/apps/api/routers.py
+++ server/apps/api/routers.py
@@ -1,7 +1,9 @@
 from rest_framework import routers
 
 from apps.test.viewsets import TestViewSet
+from apps.users.viewsets.viewset import UserViewSet
 
 
 router = routers.DefaultRouter()
 router.register('test', TestViewSet, basename='test')
+router.register('users', UserViewSet, basename='users')
