a923fa7cbc5a7c43628d708b3b0f5369a46858af
lefuq <lefuq@DESKTOP-06BGU16.localdomain>
2020-08-07 00:16:01 +0700

    перенос строк

--- server/apps/catering/models/ingredient.py
+++ server/apps/catering/models/ingredient.py
@@ -3,6 +3,6 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, blank=True)
     callories = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-
+    
     def __str__(self):
         return self.name
--- server/apps/catering/serializers/ingrserial.py
+++ server/apps/catering/serializers/ingrserial.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from apps.catering.models.ingredient import Ingredient
 
 class IngrSerializer(ModelSerializer):
