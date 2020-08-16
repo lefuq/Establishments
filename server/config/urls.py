8b04c9dff2ff14916616357aaf591eab0c91e579
lefuq <lefuq@DESKTOP-06BGU16.localdomain>
2020-08-16 22:48:20 +0700

    9ый таск

--- server/config/urls.py
+++ server/config/urls.py
@@ -2,13 +2,32 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from apps.api.routers import router
+from rest_framework.authtoken import views
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Catering API",
+      default_version='v1',
+      description="Приложение для создания заведений общественного питания",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+        ),
+    path('api/auth/token/', views.obtain_auth_token, name='token')
 ]
 
 if settings.DEBUG:
